03198cb287a93a4c9a909b2229ffb3cc82ce031e
Nelson Sambula <nelson.sambula@movitext.com>
2024-07-13 13:09:10 -0600

    chore: Update Navbar component styles to use new color scheme

--- src/layout/components/Navbar.jsx
+++ src/layout/components/Navbar.jsx
@@ -1,5 +1,5 @@
 export const Navbar = () => {
-return <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+return <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-slate-800 dark:border-slate-700">
   <div className="px-3 py-3 lg:px-5 lg:pl-3">
     <div className="flex items-center justify-between">
       <div className="flex items-center justify-start rtl:justify-end">
