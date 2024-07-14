b13c95936c15801c18b20fce48681ca475c533f7
Nelson Sambula <nelson.sambula@movitext.com>
2024-07-14 12:02:26 -0600

    refactor: Improve CSS file formatting and add support for number input styling

--- src/orders/components/NewOrderComponent.jsx
+++ src/orders/components/NewOrderComponent.jsx
@@ -1,16 +1,67 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 export const NewOrderComponent = ({ handleShowModal }) => {
+  const [formValues, setFormValues] = useState({
+    orderType: '',
+    supplier: '',
+    category: '',
+    product: '',
+    quantity: 0,
+    price: 0
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Validate form
+    const { orderType, supplier, category, product, quantity, price } =
+      formValues
+
+    if (
+      orderType === '' ||
+      supplier === '' ||
+      category === '' ||
+      product === '' ||
+      quantity === 0 ||
+      price === 0
+    ) {
+      return
+    }
+    // Save order in local storage
+    const orders = JSON.parse(localStorage.getItem('orders')) || []
+    const newOrder = {
+      id: orders.length + 1,
+      orderType,
+      supplier,
+      category,
+      product,
+      quantity,
+      price
+    }
+    orders.push(newOrder)
+    localStorage.setItem('orders', JSON.stringify(orders))
+    // Close modal
+    handleShowModal(false)
+  }
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
-      <div className="relative p-4 w-full max-w-md max-h-full bg-white rounded-lg shadow dark:bg-gray-800">
-        <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-100">
-          <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+    <div className="new-order fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
+      <div className="new-order__container relative p-4 w-full max-w-md max-h-full bg-white rounded-lg shadow dark:bg-gray-800">
+        <div className="new-order__header flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-100">
+          <h3 className="new-order__title text-xl font-semibold text-gray-900 dark:text-white">
             Nuevo Pedido
           </h3>
           <button
             type="button"
-            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            className="new-order__close-btn text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
             onClick={() => handleShowModal(false)}
           >
             <svg
@@ -31,106 +82,134 @@ export const NewOrderComponent = ({ handleShowModal }) => {
             <span className="sr-only">Close modal</span>
           </button>
         </div>
-        <div className="p-4 md:p-5">
-          <form className="space-y-4" action="#">
-            <div>
+        <div className="new-order__body p-4 md:p-5">
+          <form className="new-order__form space-y-4" onSubmit={handleSubmit}>
+            <div className="new-order__form-group">
               <label
-                htmlFor="default"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="typeOrder"
+                className="new-order__label block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Tipo de pedido
               </label>
               <select
-                id="default"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                id="typeOrder"
+                name="orderType"
+                value={formValues.orderType}
+                onChange={handleChange}
+                className="new-order__input bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option selected>Selecciona un tipo de pedido</option>
-                <option value="1">Pedido de compra</option>
-                <option value="2">Pedido de venta</option>
+                <option value="compra">Pedido de compra</option>
+                <option value="venta">Pedido de venta</option>
               </select>
             </div>
-            <div>
+            <div className="new-order__form-group">
               <label
-                htmlFor="default"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="supplier"
+                className="new-order__label block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Proveedor
               </label>
               <select
-                id="default"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                id="supplier"
+                name="supplier"
+                value={formValues.supplier}
+                onChange={handleChange}
+                className="new-order__input bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option selected>Selecciona un proveedor</option>
-                <option value="1">Amazon</option>
-                <option value="2">Mercado Libre</option>
-                <option value="3">Walmart</option>
+                <option value="amazon">Amazon</option>
+                <option value="mercado-libre">Mercado Libre</option>
+                <option value="walmart">Walmart</option>
               </select>
             </div>
-            <div>
+            <div className="new-order__form-group">
               <label
-                htmlFor="default"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="category"
+                className="new-order__label block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Categoria
               </label>
               <select
-                id="default"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                id="category"
+                name="category"
+                value={formValues.category}
+                onChange={handleChange}
+                className="new-order__input bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option selected>Selecciona una categoria</option>
-                <option value="1">Electronica</option>
-                <option value="2">Ropa</option>
-                <option value="3">Hogar</option>
+                <option value="electronica">Electronica</option>
+                <option value="ropa">Ropa</option>
+                <option value="hogar">Hogar</option>
               </select>
             </div>
-            <div>
+            <div className="new-order__form-group">
               <label
-                htmlFor="default"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="product"
+                className="new-order__label block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Producto
               </label>
               <select
-                id="default"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                id="product"
+                name="product"
+                value={formValues.product}
+                onChange={handleChange}
+                className="new-order__input bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option selected>Selecciona un producto</option>
-                <option value="1">Laptop</option>
-                <option value="2">Camisa</option>
-                <option value="3">Mesa</option>
+                <option value="laptop">Laptop</option>
+                <option value="camisa">Camisa</option>
+                <option value="mesa">Mesa</option>
               </select>
             </div>
-            <div>
+            <div className="new-order__form-group">
               <label
-                htmlFor="default"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="quantity"
+                className="new-order__label block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Cantidad
               </label>
               <input
-                id="default"
+                id="quantity"
+                name="quantity"
+                value={formValues.quantity}
+                onChange={handleChange}
+                onKeyDown={(e) => {
+                  if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+                    e.preventDefault()
+                  }
+                }}
                 type="number"
                 placeholder="Ingresa la cantidad"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="new-order__input bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
-            <div>
+            <div className="new-order__form-group">
               <label
-                htmlFor="default"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="price"
+                className="new-order__label block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
                 Precio
               </label>
               <input
-                id="default"
+                id="price"
+                name="price"
+                value={formValues.price}
+                onChange={handleChange}
+                onKeyDown={(e) => {
+                  if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+                    e.preventDefault()
+                  }
+                }}
                 placeholder="Ingresa el precio"
                 type="number"
-                className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="new-order__input bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
             <button
               type="submit"
-              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="new-order__submit-btn w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Crear Pedido
             </button>
--- src/orders/pages/OrdersPage.jsx
+++ src/orders/pages/OrdersPage.jsx
@@ -1,13 +1,19 @@
-// import { useState } from 'react'
-import { NewOrderComponent } from '../components/NewOrderComponent'
-import { TitlePageComponent } from '../../shared/components/TitlePageComponent'
-import { useShowModal } from '../../shared/hooks/useShowModal'
+import { NewOrderComponent } from '../components/NewOrderComponent';
+import { TitlePageComponent } from '../../shared/components/TitlePageComponent';
+import { useShowModal } from '../../shared/hooks/useShowModal';
+import { useEffect, useState } from 'react';
 
 export const OrdersPage = () => {
-  const { showModal, handleShowModal } = useShowModal()
+  const { showModal, handleShowModal } = useShowModal();
+  const [ordersList, setOrdersList] = useState([]);
+
+  useEffect(() => {
+    const orders = JSON.parse(localStorage.getItem('orders')) || [];
+    setOrdersList(orders);
+  }, []);
 
   return (
-    <div className="container">
+    <div className="orders-page">
       <TitlePageComponent
         titleName="Pedidos"
         buttonTitle="Nuevo Pedido"
@@ -15,12 +21,12 @@ export const OrdersPage = () => {
       />
       {showModal && <NewOrderComponent handleShowModal={handleShowModal} />}
 
-      <div className="mt-10">
-        <label htmlFor="table-search" className="sr-only">
-          Search
+      <div className="orders-page__search mt-10">
+        <label htmlFor="table-search" className="orders-page__search-label sr-only">
+          Buscar
         </label>
-        <div className="relative">
-          <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+        <div className="orders-page__search-input-wrapper relative">
+          <div className="orders-page__search-icon absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
             <svg
               className="w-4 h-4 text-gray-500 dark:text-gray-400"
               aria-hidden="true"
@@ -40,132 +46,125 @@ export const OrdersPage = () => {
           <input
             type="text"
             id="table-search"
-            className="block py-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="orders-page__search-input block py-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Buscar..."
           />
         </div>
       </div>
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-2 rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+
+      <div className="orders-page__table-container relative overflow-x-auto shadow-md sm:rounded-lg mt-2 rounded-lg">
+        <table className="orders-page__table w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="orders-page__table-head text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" className="px-4 py-3">
-                Codigo de Pedido
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Tipo de Pedido
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Proveedor
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Cliente
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Categoria
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Precio
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Cantidad
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Estado
-              </th>
-              <th scope="col" className="px-8 py-3">
-                Acciones
-              </th>
+              <th scope="col" className="orders-page__table-header px-4 py-3">Id Pedido</th>
+              <th scope="col" className="orders-page__table-header px-6 py-3">Tipo de Pedido</th>
+              <th scope="col" className="orders-page__table-header px-6 py-3">Proveedor</th>
+              <th scope="col" className="orders-page__table-header px-6 py-3">Cliente</th>
+              <th scope="col" className="orders-page__table-header px-6 py-3">Categoria</th>
+              <th scope="col" className="orders-page__table-header px-6 py-3">Precio</th>
+              <th scope="col" className="orders-page__table-header px-6 py-3">Cantidad</th>
+              <th scope="col" className="orders-page__table-header px-6 py-3">Estado</th>
+              <th scope="col" className="orders-page__table-header px-8 py-3">Acciones</th>
             </tr>
           </thead>
           <tbody>
-            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <th
-                scope="row"
-                className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            {ordersList.map((order) => (
+              <tr
+                key={order.id}
+                className="orders-page__table-row bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
               >
-                P0001
-              </th>
-              <td className="px-6 py-4">Compra</td>
-              <td className="px-6 py-4">Apple</td>
-              <td className="px-6 py-4">n/a</td>
-              <td className="px-6 py-4">Electronica</td>
-              <td className="px-6 py-4">$2999</td>
-              <td className="px-6 py-4">100</td>
-              <td className="px-8 py-4">
-                <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-green-100 bg-green-600 rounded-full">
-                  Completado
-                </span>
-              </td>
-              <td className="px-6 py-4">
-                {/* Trash */}
-                <button
-                  type="button"
-                  className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-normal rounded-md text-sm  text-center inline-flex items-center me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                <th
+                  scope="row"
+                  className="orders-page__table-cell px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  <svg
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    fill="none"
-                    viewBox="0 0 24 24"
+                  {order.id}
+                </th>
+                <td className="orders-page__table-cell px-6 py-4">{order.orderType}</td>
+                <td className="orders-page__table-cell px-6 py-4">{order.supplier}</td>
+                <td className="orders-page__table-cell px-6 py-4">{order.customer}</td>
+                <td className="orders-page__table-cell px-6 py-4">{order.category}</td>
+                <td className="orders-page__table-cell px-6 py-4">${Number(order.price)}</td>
+                <td className="orders-page__table-cell px-6 py-4">{Number(order.quantity)}</td>
+                <td className="orders-page__table-cell px-8 py-4">
+                  {order.id % 2 === 0 ? (
+                    <span className="orders-page__status-completed inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-green-100 bg-green-600 rounded-full">
+                      Completado
+                    </span>
+                  ) : (
+                    <span className="orders-page__status-pending inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-orange-600 rounded-full">
+                      Pendiente
+                    </span>
+                  )}
+                </td>
+                <td className="orders-page__table-cell px-6 py-4">
+                  <button
+                    type="button"
+                    className="orders-page__button-delete text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-normal rounded-md text-sm text-center inline-flex items-center me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
                   >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    ></path>
-                  </svg>
-                  <span className="sr-only">Icon description</span>
-                </button>
-                {/* Edit */}
-                <button
-                  type="button"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-md text-sm  text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  <svg
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    fill="none"
-                    viewBox="0 0 24 24"
+                    <svg
+                      className="w-5 h-5"
+                      aria-hidden="true"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      ></path>
+                    </svg>
+                    <span className="sr-only">Eliminar</span>
+                  </button>
+
+                  <button
+                    type="button"
+                    className="orders-page__button-edit text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-md text-sm text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M15 3l6 6-9 9-6-6 9-9Z"
-                    ></path>
-                  </svg>
-                  <span className="sr-only">Icon description</span>
-                </button>
-                {/* Completado */}
-                <button
-                  type="button"
-                  className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-normal rounded-md text-sm  text-center inline-flex items-center me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                >
-                  <svg
-                    className="w-5 h-5"
-                    aria-hidden="true"
-                    fill="none"
-                    viewBox="0 0 24 24"
+                    <svg
+                      className="w-5 h-5"
+                      aria-hidden="true"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 3l6 6-9 9-6-6 9-9Z"
+                      ></path>
+                    </svg>
+                    <span className="sr-only">Editar</span>
+                  </button>
+
+                  <button
+                    type="button"
+                    className="orders-page__button-complete text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-normal rounded-md text-sm text-center inline-flex items-center me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                   >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M5 13l4 4L19 7"
-                    ></path>
-                  </svg>
-                  <span className="sr-only">Icon description</span>
-                </button>
-              </td>
-            </tr>
+                    <svg
+                      className="w-5 h-5"
+                      aria-hidden="true"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M5 13l4 4L19 7"
+                      ></path>
+                    </svg>
+                    <span className="sr-only">Completar</span>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
