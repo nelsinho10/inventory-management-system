b14f62ebfe2a5638da4db962a84857a69bfe5930
Nelson Sambula <nelson.sambula@movitext.com>
2024-07-13 18:39:53 -0600

    feat: Improve OrdersPage layout and add search functionality

--- src/orders/pages/OrdersPage.jsx
+++ src/orders/pages/OrdersPage.jsx
@@ -1,12 +1,168 @@
+import { NavLink } from 'react-router-dom'
+
 export const OrdersPage = () => {
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
-      <h1 className="text-3xl font-semibold text-gray-800 dark:text-white">
-        Orders
-      </h1>
-      <p className="mt-4 text-gray-600 dark:text-gray-400">
-        You have no orders yet.
-      </p>
+    <div className="container">
+      <div className="flex justify-between p-3 max-w-full dark:bg-gray-700 dark:border-gray-700 rounded-lg">
+        <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
+          Pedidos
+        </h1>
+        <NavLink
+          to={'/pedidos/nuevo'}
+          type="button"
+          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Nuevo Pedido
+        </NavLink>
+      </div>
+
+      <div className="mt-10">
+        <label htmlFor="table-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            className="block py-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Buscar..."
+          />
+        </div>
+      </div>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-2 rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-4 py-3">
+                Codigo de Pedido
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Tipo de Pedido
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Proveedor
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Cliente
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Categoria
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Precio
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Estado
+              </th>
+              <th scope="col" className="px-8 py-3">
+                Acciones
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <th
+                scope="row"
+                className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                P0001
+              </th>
+              <td className="px-6 py-4">Compra</td>
+              <td className="px-6 py-4">Apple</td>
+              <td className="px-6 py-4">n/a</td>
+              <td className="px-6 py-4">Electronica</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-8 py-4">
+                <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-green-100 bg-green-600 rounded-full">
+                  Completado
+                </span>
+              </td>
+              <td className="px-6 py-4">
+                {/* Trash */}
+                <button
+                  type="button"
+                  className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-normal rounded-md text-sm  text-center inline-flex items-center me-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+                >
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Icon description</span>
+                </button>
+                {/* Edit */}
+                <button
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-md text-sm  text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 3l6 6-9 9-6-6 9-9Z"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Icon description</span>
+                </button>
+                {/* Completado */}
+                <button
+                  type="button"
+                  className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-normal rounded-md text-sm  text-center inline-flex items-center me-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                >
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Icon description</span>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
